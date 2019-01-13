9f06b376d17f017719667e55577c6d1e6a795075
Kim Friis Hansen <kim@kimfriishansen.dk>
2019-01-13 03:35:09 +0100

    Add files via upload

---
+++ react2-fundamental-workshop/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import AppExcercises from './AppExcercises';
+
+ReactDOM.render(<AppExcercises />, document.getElementById('root'));
